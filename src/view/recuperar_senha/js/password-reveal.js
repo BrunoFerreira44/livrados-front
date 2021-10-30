0771092e5fa64d02774b08d79df0c7cd0702c591
Bruno Ferreira <brunoferreira1999@live.com>
2021-10-29 23:49:51 -0300

    Integração com o backend

--- src/view/recuperar_senha/js/password-reveal.js
+++
@@ -1,9 +0,0 @@
-const txtPassword = document.getElementById("password");
-
-document.getElementById("pass-reveal-png").addEventListener("click", () => {
-   if (txtPassword.type === "password") {
-      txtPassword.type = "text";
-   } else {
-      txtPassword.type = "password";
-   }
-});
