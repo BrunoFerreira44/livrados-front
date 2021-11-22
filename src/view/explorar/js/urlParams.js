d4d9f17f0657bbc9281f38f8febbb6888c496349
Bruno Ferreira <brunoferreira1999@live.com>
2021-11-22 19:40:42 -0300

    Outra tentativa de parametros na url

--- src/view/explorar/js/urlParams.js
+++ src/view/explorar/js/urlParams.js
@@ -1,9 +1,6 @@
-console.log("MEU DEUSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-console.log("Tentativa de colocar o windows search === " + window.location.search)
 const urlParams = new URLSearchParams(window.location.search);
 
 if (urlParams){
-    console.log("Entrei aqui");
 
     const searchFor = urlParams.get("titulo").replace('_', ' ');
     console.log("SearchFor == " + searchFor);
