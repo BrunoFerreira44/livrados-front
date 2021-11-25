e41e89a8197e52e4318d2b1dd23bd118d69f5bad
Bruno Ferreira <brunoferreira1999@live.com>
2021-11-25 00:15:36 -0300

    Implementação da busca da pagina principal

--- src/js/pagina_principal.js
+++ src/js/pagina_principal.js
@@ -6,3 +6,18 @@ btnSair.addEventListener('click', () => {
    window.location.replace('https://livrados.vercel.app/');
 
 });
+
+
+//#######################################################################
+
+const txtPesquisar = document.querySelector('#txtPesquisar');
+const btnPesquisar = document.querySelector('#btnPesquisar');
+
+btnPesquisar.addEventListener('click', () => {
+   
+   if (!txtPesquisar.value)
+      return;
+
+   const setterUrlParameter = txtPesquisar.value.replace(' ', '_');
+   window.location.replace(`https://livrados.vercel.app/view/explorar/index.html?title=${setterUrlParameter}`);
+})
