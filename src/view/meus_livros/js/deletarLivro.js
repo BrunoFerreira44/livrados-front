35a5cdfd26c72bcadab7cd342d54c0d3a7d1d104
Bruno Ferreira <brunoferreira1999@live.com>
2021-12-01 18:38:55 -0300

    tentativa deleção

--- src/view/meus_livros/js/deletarLivro.js
+++ src/view/meus_livros/js/deletarLivro.js
@@ -1,46 +1,50 @@
-const btnExcluir = document.querySelector('#btnExcluir');
+const btnDetalhes = document.querySelector('#btnDetalhes');
 
-btnExcluir.addEventListener('click', async() => {
+btnDetalhes.addEventListener('click', async() => {
 
+    const btnExcluir = document.querySelector('#btnExcluir');
     const userId = localStorage.user_id;
     const title = document.querySelector('#titlePopup').value.substring(8);
 
     console.log('User ID === ' + userId);
     console.log('Titulo === ' + title);
 
-    const requestParams = {
-        method: "POST",
-        body: JSON.stringify({
-            userIdParam: userId,
-            titleParam: title
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    }
+    btnExcluir.addEventListener('click', async() => {
 
-    try {
-
-        const fetchDelete = await fetch('https://livrados-backend.herokuapp.com/deleteBook', requestParams);
-        const fetchResult = await fetchDelete.json();
-
-        if(!fetchResult.success){
+        const requestParams = {
+            method: "POST",
+            body: JSON.stringify({
+                userIdParam: userId,
+                titleParam: title
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        }
+    
+        try {
+    
+            const fetchDelete = await fetch('https://livrados-backend.herokuapp.com/deleteBook', requestParams);
+            const fetchResult = await fetchDelete.json();
+    
+            if(!fetchResult.success){
+                return Swal.fire({
+                    icon: "error",
+                    /* title: "Opa..", */
+                    text: "Não foi possível deletar o livro. Tente novamente!",
+                    showConfirmButton: false,
+                    timer: 2000,
+                });
+            }
+    
             return Swal.fire({
-                icon: "error",
+                icon: "success",
                 /* title: "Opa..", */
-                text: "Não foi possível deletar o livro. Tente novamente!",
+                text: "Livro excluído com sucesso!",
                 showConfirmButton: false,
                 timer: 2000,
             });
+            
+        } catch (err) {
+            console.error("Erro ao acessar endpoint de deleção de livro : " + err);
         }
-
-        return Swal.fire({
-            icon: "success",
-            /* title: "Opa..", */
-            text: "Livro excluído com sucesso!",
-            showConfirmButton: false,
-            timer: 2000,
-        });
-        
-    } catch (err) {
-        console.error("Erro ao acessar endpoint de deleção de livro : " + err);
-    }
+    })
 })
